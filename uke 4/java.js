94bfeed60365e6b2938b828748a15f1f461e549c
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2021-04-19 10:59:52 +0200

    Add files via upload

---
+++ uke 4/java.js
@@ -0,0 +1,24 @@
+
+function opp(){
+    view();
+     if(personer == 5){
+        alert('NÅ ER DET FULT, VENNLIGST VENT')
+}
+    if (personer >= 0 && personer < 4){
+        personer ++
+}}
+ 
+
+ function ned(){ 
+    if (personer >= 1){
+        personer --
+     } 
+ view();        
+}
+
+
+function restart(){
+    personer = 0;
+    view();
+
+}
